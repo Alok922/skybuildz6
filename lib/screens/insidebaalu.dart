528696dc27902a22c34aa4a6f4dbd474207c4e21
Alok Singh <63591428+Alok922@users.noreply.github.com>
2020-08-14 11:20:07 -0700

    Add  files

---
+++ lib/screens/insidebaalu.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+class BaaluDetails extends StatefulWidget {
+  @override
+  _BaaluDetailsState createState() => _BaaluDetailsState();
+}
+
+class _BaaluDetailsState extends State<BaaluDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF4F51C0),
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0.1,
+        backgroundColor: Color(0xFF4F51C0),
+        title: Text('Profile'),
+      ),
+      body: Body(),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemImage(
+          imgSrc: "assets/img/sand.jpg",
+        ),
+        Expanded(
+          child: ItemInfo(),
+        ),
+      ],
+    );
+  }
+}
+
+class ItemInfo extends StatelessWidget {
+  const ItemInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      child: Column(
+        children: <Widget>[
+          shopeName(name: "Product Details"),
+          TitlePriceRating(
+            name: "Arach Plaster Baalu",
+            numOfReviews: 24,
+            rating: 4,
+            onRatingChanged: (value) {},
+          ),
+          Text(
+            "Nowadays, making printed materials have become fast, easy and simple. If you want your promotional material to be an eye-catching object, you should make it colored. By way of using inkjet printer this is not hard to make. An inkjet printer is any printer that places extremely small droplets of ink onto paper to create an image.",
+            style: TextStyle(
+              height: 1.5,
+            ),
+          ),
+          SizedBox(height: size.height * 0.1),
+          // Free space  10% of total height
+          OrderButton(
+            size: size,
+            press: () {},
+          )
+        ],
+      ),
+    );
+  }
+
+  Row shopeName({String name}) {
+    return Row(
+      children: <Widget>[
+        Icon(
+          Icons.location_on,
+          color: white,
+        ),
+        SizedBox(width: 10),
+        Text(name),
+      ],
+    );
+  }
+}
+
+class ItemImage extends StatelessWidget {
+  final String imgSrc;
+  const ItemImage({
+    Key key,
+    this.imgSrc,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Image.asset(
+      imgSrc,
+      height: size.height * 0.25,
+      width: double.infinity,
+      // it cover the 25% of total height
+      fit: BoxFit.fill,
+    );
+  }
+}
+
+class OrderButton extends StatelessWidget {
+  const OrderButton({
+    Key key,
+    @required this.size,
+    this.press,
+  }) : super(key: key);
+
+  final Size size;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // padding: EdgeInsets.all(20),
+      width: size.width * 0.8,
+      // it will cover 80% of total width
+      decoration: BoxDecoration(
+        color: Color(0xFF4F51C0),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: press,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Send Inquiry",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
